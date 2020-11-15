2c28cccaecdbd74a5414a7b28ec35203de85f980
fonzerelly <christian.hoerauf@gmail.com>
2020-11-15 23:48:53 +0100

    implements buzz

--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -2,6 +2,9 @@ const fizzbuzz = (num) => {
     if (num % 3 === 0) {
         return "fizz"
     }
+    if (num % 5 === 0) {
+        return "buzz"
+    }
     return String(num)
 }
 
--- src/fizzbuzz.spec.js
+++ src/fizzbuzz.spec.js
@@ -7,7 +7,9 @@ describe('fizzbuzz', () => {
         {input: 1, output: "1"},
         {input: 2, output: "2"},
         {input: 3, output: "fizz"},
-        {input: 6, output: "fizz"}
+        {input: 6, output: "fizz"},
+        {input: 5, output: "buzz"},
+        {input: 10, output: "buzz"}
     ].forEach(({input, output}) => {
         it(`should turn ${input} to "${output}"`, () => {
             expect(fizzbuzz(input)).toBe(output)
